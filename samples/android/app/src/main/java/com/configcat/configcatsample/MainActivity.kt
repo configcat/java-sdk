7a378dbaa0aa66320e3f96df565ae6f7d76fa605
ConfigCat <developer@configcat.com>
2018-10-07 23:43:26 +0200

    Updated samples

--- samples/android/app/src/main/java/com/configcat/configcatsample/MainActivity.kt
+++ samples/android/app/src/main/java/com/configcat/configcatsample/MainActivity.kt
@@ -3,10 +3,7 @@ package com.configcat.configcatsample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.configcat.AutoPollingPolicy
-import com.configcat.ConfigCatClient
-import com.configcat.ConfigCache
-import com.configcat.ConfigFetcher
+import com.configcat.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,23 +12,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         ConfigCatClient.newBuilder()
-                .refreshPolicy({fetcher: ConfigFetcher, cache: ConfigCache -> AutoPollingPolicy.newBuilder()
-                        .autoPollRateInSeconds(5)
-                        .configurationChangeListener({parser, newConfiguration ->
+                .refreshPolicy { fetcher: ConfigFetcher, cache: ConfigCache -> AutoPollingPolicy.newBuilder()
+                        .autoPollIntervalInSeconds(5)
+                        .configurationChangeListener { parser, newConfiguration ->
                             run {
                                 // create a user object to identify the caller
                                 val user = User.newBuilder()
                                         .build("key")
 
+
                                 var config = parser.parse(Sample::class.java, newConfiguration, user)
                                 var textField = findViewById<TextView>(R.id.editText)
                                 textField.text = "bool30TrueAdvancedRules: " + config.bool30TrueAdvancedRules + "\n" +
-                                        "integer25One25Two25Three25FourAdvancedRules: " + config.keyInteger + "\n" +
+                                        "integer25One25Two25Three25FourAdvancedRules: " + config.integer25One25Two25Three25FourAdvancedRules + "\n" +
                                         "double25Pi25E25Gr25Zero: " + config.double25Pi25E25Gr25Zero + "\n" +
-                                        "string25Cat25Dog25Falcon25Horse: " + config.keyString
+                                        "string25Cat25Dog25Falcon25Horse: " + config.string25Cat25Dog25Falcon25Horse
                             }
-                        })
-                        .build(fetcher, cache)})
+                        }
+                        .build(fetcher, cache)}
                 .build("PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A")
     }
 
