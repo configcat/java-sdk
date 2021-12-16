41a8907976ad08cdc7d6f53433a11fd4eaff92f9
Peter Csajtai <peter.csajtai@outlook.com>
2021-12-16 11:51:18 +0100

    Update samples [skip ci]

--- samples/console/src/main/java/Main.java
+++ samples/console/src/main/java/Main.java
@@ -7,15 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
 
-        ConfigCatClient client = ConfigCatClient
-                .newBuilder()
+        ConfigCatClient client = ConfigCatClient.newBuilder()
+                // Info level logging helps to inspect the feature flag evaluation process.
+                // Use the default Warning level to avoid too detailed logging in your application.
+                .logLevel(LogLevel.INFO)
                 .build("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ");
 
-        // get individual config values identified by a key for a user
+        // Get individual config values identified by a key for a user.
         System.out.println("isAwesomeFeatureEnabled: " + client.getValue(String.class, "isAwesomeFeatureEnabled", ""));
 
 
-        // create a user object to identify the caller
+        // Create a user object to identify the caller.
         User user = User.newBuilder()
                 .email("configcat@example.com")
                 .build("key");
