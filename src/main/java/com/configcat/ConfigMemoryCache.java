1e35bd8e489d8fc9bd2ae917c1bb5bdc9ab20836
Peter Csajtai <peter.csajtai@outlook.com>
2022-01-12 14:20:53 +0100

    Remove murmur hash due to compatibility reasons

--- src/main/java/com/configcat/ConfigMemoryCache.java
+++ src/main/java/com/configcat/ConfigMemoryCache.java
@@ -2,13 +2,10 @@ package com.configcat;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.apache.commons.codec.digest.MurmurHash3;
-
-import java.util.Arrays;
 
 class ConfigMemoryCache {
     private Config cached;
-    private long[] cachedHash;
+    private String cachedJson;
     private final Gson gson = new GsonBuilder().create();
     private final ConfigCatLogger logger;
 
@@ -21,17 +18,14 @@ class ConfigMemoryCache {
             return null;
         }
 
-        if (this.cachedHash != null) {
-            long[] hash = MurmurHash3.hash128(json);
-            if (Arrays.equals(this.cachedHash, hash)) {
-                return this.cached;
-            }
+        if (this.cachedJson != null && this.cachedJson == json) {
+            return this.cached;
         }
 
         try {
             this.cached = this.gson.fromJson(json, Config.class);
             this.cached.jsonString = json;
-            this.cachedHash = MurmurHash3.hash128(json);
+            this.cachedJson = json;
             return cached;
         } catch (Exception e) {
             this.logger.error("Config JSON parsing failed.", e);
