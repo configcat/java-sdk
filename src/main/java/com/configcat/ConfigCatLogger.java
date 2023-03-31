181cdf44bc092d8ef0f9b0a5cf3d5ba2e9422711
novalisdenahi <novalisdenahi@gmail.com>
2023-03-31 14:16:12 +0200

    Fixing log code smells

--- src/main/java/com/configcat/ConfigCatLogger.java
+++ src/main/java/com/configcat/ConfigCatLogger.java
@@ -23,27 +23,27 @@ class ConfigCatLogger {
 
     public void warn(int eventId, String message) {
         if (this.logLevel.ordinal() <= LogLevel.WARNING.ordinal()) {
-            this.logger.warn("[" + eventId + "] " + message);
+            this.logger.warn("[{}] {}", eventId, message);
         }
     }
 
     public void error(int eventId, String message, Exception exception) {
         if (this.configCatHooks != null) this.configCatHooks.invokeOnError(message);
         if (this.logLevel.ordinal() <= LogLevel.ERROR.ordinal()) {
-            this.logger.error(String.format("[%d] %s",eventId, message), exception);
+            this.logger.error("[{}] {}",eventId, message, exception);
         }
     }
 
     public void error(int eventId, String message) {
         if (this.configCatHooks != null) this.configCatHooks.invokeOnError(message);
         if (this.logLevel.ordinal() <= LogLevel.ERROR.ordinal()) {
-            this.logger.error(String.format("[%d] %s",eventId, message));
+            this.logger.error("[{}] {}",eventId, message);
         }
     }
 
     public void info(int eventId, String message) {
         if (this.logLevel.ordinal() <= LogLevel.INFO.ordinal()) {
-            this.logger.info(String.format("[%d] %s",eventId, message));
+            this.logger.info("[{}] {}",eventId, message);
         }
     }
 
--- src/test/java/com/configcat/LoggerTests.java
+++ src/test/java/com/configcat/LoggerTests.java
@@ -3,6 +3,8 @@ package com.configcat;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 
+
+
 import static org.mockito.Mockito.*;
 
 public class LoggerTests {
@@ -14,12 +16,12 @@ public class LoggerTests {
         logger.debug("debug");
         logger.info(5000, "info");
         logger.warn(3000, "warn");
-        logger.error(1000, "error");
+        logger.error(1000, "error", new Exception());
 
         verify(mockLogger, times(1)).debug("[0] debug");
-        verify(mockLogger, times(1)).error("[1000] error");
-        verify(mockLogger, times(1)).warn("[3000] warn");
-        verify(mockLogger, times(1)).info("[5000] info");
+        verify(mockLogger, times(1)).error(anyString(), eq(1000),eq("error"), any(Exception.class));
+        verify(mockLogger, times(1)).warn(anyString(), eq(3000),eq("warn"));
+        verify(mockLogger, times(1)).info(anyString(), eq(5000), eq("info"));
     }
 
     @Test
@@ -30,12 +32,12 @@ public class LoggerTests {
         logger.debug("debug");
         logger.info(5000, "info");
         logger.warn(3000, "warn");
-        logger.error(1000, "error");
+        logger.error(1000, "error", new Exception());
 
         verify(mockLogger, never()).debug("[0] debug");
-        verify(mockLogger, times(1)).error("[1000] error");
-        verify(mockLogger, times(1)).warn("[3000] warn");
-        verify(mockLogger, times(1)).info("[5000] info");
+        verify(mockLogger, times(1)).error(anyString(), eq(1000),eq("error"), any(Exception.class));
+        verify(mockLogger, times(1)).warn(anyString(), eq(3000),eq("warn"));
+        verify(mockLogger, times(1)).info(anyString(), eq(5000), eq("info"));
     }
 
     @Test
@@ -46,12 +48,12 @@ public class LoggerTests {
         logger.debug("debug");
         logger.info(5000, "info");
         logger.warn(3000, "warn");
-        logger.error(1000, "error");
+        logger.error(1000, "error", new Exception());
 
         verify(mockLogger, never()).debug("[0] debug");
-        verify(mockLogger, never()).info("[5000] info");
-        verify(mockLogger, times(1)).warn("[3000] warn");
-        verify(mockLogger, times(1)).error("[1000] error");
+        verify(mockLogger, never()).info(anyString(), eq(5000), eq("info"));
+        verify(mockLogger, times(1)).warn(anyString(), eq(3000),eq("warn"));
+        verify(mockLogger, times(1)).error(anyString(), eq(1000),eq("error"), any(Exception.class));
     }
 
     @Test
@@ -62,12 +64,14 @@ public class LoggerTests {
         logger.debug("debug");
         logger.info(5000, "info");
         logger.warn(3000, "warn");
-        logger.error(1000, "error");
+        logger.error(1000, "error", new Exception());
 
         verify(mockLogger, never()).debug("[0] debug");
-        verify(mockLogger, never()).info("[5000] info");
-        verify(mockLogger, never()).warn("[3000] warn");
-        verify(mockLogger, times(1)).error("[1000] error");
+        verify(mockLogger, never()).info(anyString(), eq(5000), eq("info"));
+        verify(mockLogger, never()).warn(anyString(), eq(3000),eq("warn"));
+        verify(mockLogger, times(1)).error(anyString(), eq(1000),eq("error"), any(Exception.class));
+
+
     }
 
     @Test
@@ -75,14 +79,14 @@ public class LoggerTests {
         Logger mockLogger = mock(Logger.class);
         ConfigCatLogger logger = new ConfigCatLogger(mockLogger, LogLevel.NO_LOG);
 
-        logger.debug("debug");
+        logger.debug("[0] debug");
         logger.info(5000, "info");
         logger.warn(3000, "warn");
-        logger.error(1000, "error");
+        logger.error(1000, "error", new Exception());
 
         verify(mockLogger, never()).debug("[0] debug");
-        verify(mockLogger, never()).info("[5000] info");
-        verify(mockLogger, never()).warn("[3000] warn");
-        verify(mockLogger, never()).error("[1000] error");
+        verify(mockLogger, never()).info(anyString(), eq(5000), eq("info"));
+        verify(mockLogger, never()).warn(anyString(), eq(3000),eq("warn"));
+        verify(mockLogger, never()).error(anyString(), eq(1000),eq("error"), any(Exception.class));
     }
 }
