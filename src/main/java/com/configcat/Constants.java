d1ce792486993d32891094a649ef79554c3d29e4
novalisdenahi <novalisdenahi@gmail.com>
2023-05-05 17:05:13 +0200

    Update version to 9.0.0

--- src/main/java/com/configcat/Constants.java
+++ src/main/java/com/configcat/Constants.java
@@ -7,5 +7,5 @@ public final class Constants {
     public static final long DISTANT_PAST = 0;
     public static final String CONFIG_JSON_NAME = "config_v5";
     public static final String SERIALIZATION_FORMAT_VERSION = "v1";
-    public static final String VERSION = "8.1.0";
+    public static final String VERSION = "9.0.0";
 }
