3fa66c1f2cc064c33dff3f7294d9530306aeceeb
novalisdenahi <novalisdenahi@gmail.com>
2024-01-11 16:19:54 +0100

    1103 log message update.

--- src/main/java/com/configcat/ConfigCatLogMessages.java
+++ src/main/java/com/configcat/ConfigCatLogMessages.java
@@ -34,7 +34,7 @@ final class ConfigCatLogMessages {
     /**
      * Log message for Fetch Failed Due To Unexpected error. The log eventId is 1103.
      */
-    public static final String FETCH_FAILED_DUE_TO_UNEXPECTED_ERROR = "Unexpected error occurred while trying to fetch config JSON.";
+    public static final String FETCH_FAILED_DUE_TO_UNEXPECTED_ERROR = "Unexpected error occurred while trying to fetch config JSON. It is most likely due to a local network issue. Please make sure your application can reach the ConfigCat CDN servers (or your proxy server) over HTTP.";
 
     /**
      * Log message for Fetch Failed Due To Invalid Sdk Key error. The log eventId is 1100.
