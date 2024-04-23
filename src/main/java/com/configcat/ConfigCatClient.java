2c6fe1b6cf76232c3499dceef004d2cb6360b99d
Tóth Dániel <novalisdenahi@gmail.com>
2024-04-23 11:40:31 +0200

    Add more getKeyAndValue test. (#46)
    Fix possible NPE.
    Update version to 9.1.1

--- src/main/java/com/configcat/ConfigCatClient.java
+++ src/main/java/com/configcat/ConfigCatClient.java
@@ -517,25 +517,29 @@ public final class ConfigCatClient implements ConfigurationProvider {
                     return new AbstractMap.SimpleEntry<>(settingKey, (T) this.parseObject(classOfT, setting.getSettingsValue(), setting.getType()));
                 }
 
-                for (TargetingRule targetingRule : setting.getTargetingRules()) {
-                    if (targetingRule.getSimpleValue() != null) {
-                        if (variationId.equals(targetingRule.getSimpleValue().getVariationId())) {
-                            return new AbstractMap.SimpleEntry<>(settingKey, (T) this.parseObject(classOfT, targetingRule.getSimpleValue().getValue(), setting.getType()));
-                        }
-                    } else if (targetingRule.getPercentageOptions() != null) {
-                        for (PercentageOption percentageRule : targetingRule.getPercentageOptions()) {
-                            if (variationId.equals(percentageRule.getVariationId())) {
-                                return new AbstractMap.SimpleEntry<>(settingKey, (T) this.parseObject(classOfT, percentageRule.getValue(), setting.getType()));
+                if(setting.getTargetingRules() != null) {
+                    for (TargetingRule targetingRule : setting.getTargetingRules()) {
+                        if (targetingRule.getSimpleValue() != null) {
+                            if (variationId.equals(targetingRule.getSimpleValue().getVariationId())) {
+                                return new AbstractMap.SimpleEntry<>(settingKey, (T) this.parseObject(classOfT, targetingRule.getSimpleValue().getValue(), setting.getType()));
                             }
+                        } else if (targetingRule.getPercentageOptions() != null) {
+                            for (PercentageOption percentageRule : targetingRule.getPercentageOptions()) {
+                                if (variationId.equals(percentageRule.getVariationId())) {
+                                    return new AbstractMap.SimpleEntry<>(settingKey, (T) this.parseObject(classOfT, percentageRule.getValue(), setting.getType()));
+                                }
+                            }
+                        } else {
+                            throw new UnsupportedOperationException("Targeting rule THEN part is missing or invalid.");
                         }
-                    } else {
-                        throw new UnsupportedOperationException("Targeting rule THEN part is missing or invalid.");
                     }
                 }
 
-                for (PercentageOption percentageRule : setting.getPercentageOptions()) {
-                    if (variationId.equals(percentageRule.getVariationId())) {
-                        return new AbstractMap.SimpleEntry<>(settingKey, (T) this.parseObject(classOfT, percentageRule.getValue(), setting.getType()));
+                if(setting.getPercentageOptions() != null) {
+                    for (PercentageOption percentageRule : setting.getPercentageOptions()) {
+                        if (variationId.equals(percentageRule.getVariationId())) {
+                            return new AbstractMap.SimpleEntry<>(settingKey, (T) this.parseObject(classOfT, percentageRule.getValue(), setting.getType()));
+                        }
                     }
                 }
             }
