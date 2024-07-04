803d045ec66e0a593b5f41b8a08f8b9f80025928
Tóth Dániel <novalisdenahi@gmail.com>
2024-07-04 12:58:19 +0200

    Exclude log by Event Id (#53)
    * Added excludeLogEventIds options to client. The logger will exclude logs if the log event id is on the provided list.
    Prepare 9.3.0 release.
    
    * Replace eventId exclude list with filter function callback option.
    
    * Rework based on code review.
    
    * Remove unused imports.
    
    * Fix method order. Add missing JavaDoc.
    
    * Update src/main/java/com/configcat/ConfigCatClient.java
    
    Co-authored-by: adams85 <31276480+adams85@users.noreply.github.com>
    
    * Fix interface visibility.
    
    ---------
    
    Co-authored-by: adams85 <31276480+adams85@users.noreply.github.com>

--- src/main/java/com/configcat/ConfigCatLogger.java
+++ src/main/java/com/configcat/ConfigCatLogger.java
@@ -6,15 +6,17 @@ class ConfigCatLogger {
     private final Logger logger;
     private final LogLevel logLevel;
     private final ConfigCatHooks configCatHooks;
+    private final LogFilterFunction filterFunction ;
 
-    public ConfigCatLogger(Logger logger, LogLevel logLevel, ConfigCatHooks configCatHooks) {
+    public ConfigCatLogger(Logger logger, LogLevel logLevel, ConfigCatHooks configCatHooks, LogFilterFunction filterFunction ) {
         this.logger = logger;
         this.logLevel = logLevel;
         this.configCatHooks = configCatHooks;
+        this.filterFunction  = filterFunction ;
     }
 
     public ConfigCatLogger(Logger logger, LogLevel logLevel) {
-        this(logger, logLevel, null);
+        this(logger, logLevel, null, null);
     }
 
     public ConfigCatLogger(Logger logger) {
@@ -22,34 +24,38 @@ class ConfigCatLogger {
     }
 
     public void warn(int eventId, String message) {
-        if (this.logLevel.ordinal() <= LogLevel.WARNING.ordinal()) {
+        if (filter(eventId,  LogLevel.WARNING, message, null)) {
             this.logger.warn("[{}] {}", eventId, message);
         }
     }
 
     public void error(int eventId, String message, Exception exception) {
         if (this.configCatHooks != null) this.configCatHooks.invokeOnError(message);
-        if (this.logLevel.ordinal() <= LogLevel.ERROR.ordinal()) {
+        if (filter(eventId,  LogLevel.ERROR, message, exception)) {
             this.logger.error("[{}] {}", eventId, message, exception);
         }
     }
 
     public void error(int eventId, String message) {
         if (this.configCatHooks != null) this.configCatHooks.invokeOnError(message);
-        if (this.logLevel.ordinal() <= LogLevel.ERROR.ordinal()) {
+        if (filter(eventId,  LogLevel.ERROR, message, null)) {
             this.logger.error("[{}] {}", eventId, message);
         }
     }
 
     public void info(int eventId, String message) {
-        if (this.logLevel.ordinal() <= LogLevel.INFO.ordinal()) {
+        if (filter(eventId,  LogLevel.INFO, message, null)) {
             this.logger.info("[{}] {}", eventId, message);
         }
     }
 
     public void debug(String message) {
-        if (this.logLevel.ordinal() <= LogLevel.DEBUG.ordinal()) {
+        if (filter(0,  LogLevel.DEBUG, message, null)) {
             this.logger.debug("[{}] {}", 0, message);
         }
+    }
+
+    private boolean filter(int eventId, LogLevel logLevel, String message, Exception exception) {
+        return this.logLevel.ordinal() <= logLevel.ordinal() && (this.filterFunction == null || this.filterFunction.apply(logLevel, eventId, message, exception));
     }
 }
---
+++ src/main/java/com/configcat/LogFilterFunction.java
@@ -0,0 +1,19 @@
+package com.configcat;
+
+/**
+ * The Log Filter Functional Interface provides a custom filter option for the ConfigCat Logger.
+ */
+@FunctionalInterface
+public interface LogFilterFunction {
+
+    /**
+     * Apply the custom filter option to the ConfigCatLogger.
+     *
+     * @param logLevel  Event severity level.
+     * @param eventId  Event identifier.
+     * @param message Message.
+     * @param exception The exception object related to the message (if any).
+     * @return True to log the event, false will leave out the log.
+     */
+     boolean apply(LogLevel logLevel, int eventId, String message, Throwable exception);
+}
